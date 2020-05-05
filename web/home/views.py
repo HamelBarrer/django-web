a62d48bac3e487effda3d2896eff13c3e218e0be
Hamel Barrero <hamel_-barrero@hotmail.com>
2020-05-05 18:49:00 -0500

    finish form login

--- web/home/views.py
+++ web/home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Comment
 
 from .forms import CommentForm
+
+from users.models import User
 
 
 class IndexView(View):
@@ -27,5 +30,23 @@ class IndexView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
-            form.errors.as_json()
             return redirect('home:index')
+
+
+class LoginView(View):
+    template_name = 'home/snippets/login.html'
+    form_class = UserCreationForm
+    model = User
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['login'] = self.form_class
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.get_context_data())
+
+    def post(self, request, *args, **kwargs):
+        login = self.form_class(request.POST)
+        if login.is_valid():
+            login.save()
