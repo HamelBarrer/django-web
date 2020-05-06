aaed5be9f99eccc23c1432029025105af1d410de
Hamel Barrero <hamel_-barrero@hotmail.com>
2020-05-06 18:36:49 -0500

    finish settings image

--- web/web/urls.py
+++ web/web/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('home.urls')),
     path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(template_name='home/index.html'), name='logout'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
